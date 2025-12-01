71f1a364960e3de5582a7a025b7902a06b030bc1
Pablo Valencia <59809785+coronapl@users.noreply.github.com>
2025-12-01 22:35:53 +0100

    chore: formatting

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,9 +1,10 @@
-import { defineCollection } from 'astro:content';
-import { glob, } from 'astro/loaders';
-import { rssSchema } from '@astrojs/rss';
+import { defineCollection } from "astro:content";
+import { glob } from "astro/loaders";
+import { rssSchema } from "@astrojs/rss";
 
 const posts = defineCollection({
-    loader: glob({ pattern: "*.md", base: "src/posts" }), schema: rssSchema
+  loader: glob({ pattern: "*.md", base: "src/posts" }),
+  schema: rssSchema,
 });
 
 export const collections = { posts };
